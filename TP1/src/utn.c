b8d4a0d0426151368e5a0e72812179c38243fbb2
magaliirina <magaliinarciso@gmail.com>
2021-04-17 17:38:21 -0300

    Trabajo Practico version 1

--- TP1/src/utn.c
+++ TP1/src/utn.c
@@ -14,6 +14,15 @@ void utn_imprimirMensaje (char* mensaje){
 	printf("%s",mensaje);
 }
 //------------------------------------------------------------------------------------------------------------
+/**
+ * \brief Da un mensaje al usuario y obtiene un numero de tipo float, cuenta con reintetosy mensaje de ERROR
+ * \param *numero: puntero a numero float
+ * \param *mensaje: puntero a mensaje
+ * \param *mensajeErrror: puntero a mensaje de error
+ * \param reintentos: cantidad de reintentos
+ * \return 0: Si logro el objetivo -1: Si hubo un problema
+ */
+
 int utn_getnumero(float* numero,char* mensaje, char* mensajeError,int reintentos){
 	int retorno=-1;
 	float bufferNumero;
@@ -36,6 +45,17 @@ int utn_getnumero(float* numero,char* mensaje, char* mensajeError,int reintentos
 	return retorno;
 }
 //----------------------------------------------------------------------------------------------------------------
+/**
+ * \brief Da un mensaje al usuario y obtiene un numero de tipo float y lo compara con un rango, cuenta con reintetos y mensaje de ERROR
+ * \param *numero: puntero a letra
+ * \param *mensaje: puntero a mensaje
+ * \param *mensajeErrror: puntero a mensaje de error
+ * \param reintentos: cantidad de reintentos
+ * \param maximo: limite superior
+ * \param minimo: limite inferior
+ * \return 0: Si logro el objetivo -1: Si hubo un problema
+ */
+
 int utn_getnumeroRango(char* numero,char* mensaje, char* mensajeError,int reintentos, int maximo, int minimo){
 	int retorno=-1;
 	char bufferNumero;
@@ -58,21 +78,51 @@ int utn_getnumeroRango(char* numero,char* mensaje, char* mensajeError,int reinte
 	return retorno;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * \brief suma dos numeros (A+B)
+ * \param operador1: numero A
+ * \param operador2: numero B
+ * \param *pResultado: puntero a resultado final
+ * \return 0
+ */
+
 int utn_suma(float operador1,float operador2,float* pResultado){
 	*pResultado=operador1+operador2;
 	return 0;
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * \brief resta dos numeros (A-B)
+ * \param operador1: numero A
+ * \param operador2: numero B
+ * \param *pResultado: puntero a resultado final
+ * \return 0
+ */
+
 int utn_resta(float operador1, float operador2, float*pResultado){
 	*pResultado=operador1-operador2;
 	return 0;
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * \brief multiplica dos numeros (A*B)
+ * \param operador1: numero A
+ * \param operador2: numero B
+ * \param *pResultado: puntero a resultado final
+ * \return 0
+ */
 int utn_multiplicacion(float operador1, float operador2, float* pResultado){
 	*pResultado=operador1*operador2;
 	return 0;
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * \brief divide dos numeros (A/B)
+ * \param operador1: numero A
+ * \param operador2: numero B
+ * \param *pResultado: puntero a resultado final
+ * \return 0
+ */
 int utn_division(float operador1,float operador2, float* pResultado){
 	int retorno=-1;
 	if(operador2!=0){
@@ -82,6 +132,12 @@ int utn_division(float operador1,float operador2, float* pResultado){
 	return retorno;
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * \brief obtiene el factorial de un numero (A*A-1*A-n)
+ * \param operador: numero A
+ * \param *pResultado: puntero a resultado final
+ * \return 0
+ */
 int utn_factorial(float operador, float*presultado){
 	int factorial=1;
 	if(operador>0){
